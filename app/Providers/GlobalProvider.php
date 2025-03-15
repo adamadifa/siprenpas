f8023440e06a6f75ec288870d7dd85f07eecf283
Adam Adifa <adamabdi.al.a@gmail.com>
2025-03-15 11:29:40 +0700

    updatE

--- app/Providers/GlobalProvider.php
+++ app/Providers/GlobalProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Izinabsen;
+use App\Models\Izinsakit;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,9 +25,23 @@ class GlobalProvider extends ServiceProvider
     public function boot(Guard $auth): void
     {
         view()->composer('*', function ($view) use ($auth) {
+            $notifikasi_izinabsen = Izinabsen::where('status', 0)->count();
+            $notifikasi_izinsakit = Izinsakit::where('status', 0)->count();
+            $data_izinabsen = Izinabsen::select('presensi_izinabsen.npp', 'nama_lengkap', DB::raw('"i" as status'), 'presensi_izinabsen.created_at')
+                ->where('presensi_izinabsen.status', 0)
+                ->join('karyawan', 'presensi_izinabsen.npp', '=', 'karyawan.npp');
+            $data_izinsakit = Izinsakit::select('presensi_izinsakit.npp', 'nama_lengkap', DB::raw('"s" as status'), 'presensi_izinsakit.created_at')
+                ->where('presensi_izinsakit.status', 0)
+                ->join('karyawan', 'presensi_izinsakit.npp', '=', 'karyawan.npp');
 
+            $data_izin = $data_izinabsen->unionAll($data_izinsakit)->get();
+            $notifikasi_ajuan_absen = $notifikasi_izinabsen  + $notifikasi_izinsakit;
             $listbulan = ['', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
             $sharedData = [
+                'notifikasi_izinabsen' => $notifikasi_izinabsen,
+                'notifikasi_izinsakit' => $notifikasi_izinsakit,
+                'notifikasi_ajuan_absen' => $notifikasi_ajuan_absen,
+                'data_izin' => $data_izin,
                 'listbulan' => $listbulan,
             ];
             View::share($sharedData);
