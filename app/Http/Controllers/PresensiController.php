5701f8b5e73b4c8618a47893711ef0cdd0ba36bd
Adam Adifa <adamabdi.al.a@gmail.com>
2025-05-01 08:33:24 +0700

    update

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -531,7 +531,7 @@ class PresensiController extends Controller
         $jam_masuk = $tanggal_presensi . " " . date('H:i', strtotime($jam_kerja->jam_masuk));
 
         $presensi_hariini = Presensi::where('npp', $karyawan->npp)
-            ->where('tanggald', $tanggal_presensi)
+            ->where('tanggal', $tanggal_presensi)
             ->first();
 
         if (in_array($status_scan, [0, 2, 4, 6, 8])) {
